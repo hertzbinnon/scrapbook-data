601b25efec90e97b874a5f150265a41e78b27483
hertz <hertz@hertz.org>
2019-04-27 14:41:41 +0800

    init at 2019-04-27 on ubuntu 18 at home

---
+++ data/20170721143040/pageview.php
@@ -0,0 +1,15 @@
+<script>this.status='ÇëµÈ´ý'</script><script language='JavaScript' type='text/javascript' src='https://vd.51cto.com/adx.js'></script>
+<script src='//log.51cto.com/imp_countall.php?date=2017-07-21&frompos=blog_art'></script>
+<img src="" border="0" width="0" height="0" /><div><img height="0" width="0" src="http://m2593.oadz.com/mp;C;2593;454;KEklijhsWnwo/qr/6Ly2+Mvf87w=?https://ad.doubleclick.net/ddm/trackimp/N5840.220611051CTO.COM/B20064185.200996682;dc_trk_aid=400988605;dc_trk_cid=90637827;ord=[timestamp];dc_lat=;dc_rdid=;tag_for_child_directed_treatment=?"/> </div><script src='//log.51cto.com/imp_count.php?adder=HP security&date=2017-07-21&adpos=biz banner-1'></script><img src="" border="0" width="0" height="0" /><div><img height="0" width="0" src="http://m2593.oadz.com/mp;C;2593;454;KEklijhsWnwo/qr/6Ly2+Mvf87w=?https://ad.doubleclick.net/ddm/trackimp/N5840.220611051CTO.COM/B20064185.200996682;dc_trk_aid=400988605;dc_trk_cid=90637827;ord=[timestamp];dc_lat=;dc_rdid=;tag_for_child_directed_treatment=?"/>
+ </div><script src='//log.51cto.com/imp_count.php?adder=HP &date=2017-07-21&adpos=biz banner-tmp'></script><script>this.status='Íê³É'</script><script>
+function refreshall(){
+	for(var i=0;i < 0;i++){
+		document.getElementById("ifr_"+i).src='http://www.51cto.com/blank.html';
+	}
+}
+</script>
+<script>
+window.onload = function(){
+setTimeout("refreshall()",5000);
+}
+</script>
---
+++ data/20171108102732/pageview.php
@@ -0,0 +1,14 @@
+<script>this.status='ÇëµÈ´ý'</script><script language='JavaScript' type='text/javascript' src='https://vd.51cto.com/adx.js'></script>
+<script src='//log.51cto.com/imp_countall.php?date=2017-11-08&frompos=blog_art'></script>
+<img src="" border="0" width="0" height="0" /><div><img height="0" width="0" src="http://m2593.oadz.com/mp;C;2593;1187;dL4N/GHgcgRaWYXAnjI0pPqn+Mw=?https://v.admaster.com.cn/i/a98031,b2084886,c360,i0,m202,8a1,8b2,h"/> </div><script src='//log.51cto.com/imp_count.php?adder=HW yun&date=2017-11-08&adpos=51cto banner-2'></script><img src="" border="0" width="0" height="0" /><div><img height="0" width="0" src="http://m2593.oadz.com/mp;C;2593;1042;Vpb9aAvS98YsAg3YzhW7/YEMWcI=?g.cn.miaozhen.com/x/k=2060126&p=79Cao&dx=__IPDX__&rt=2&ns=__IP__&ni=__IESID__&v=__LOC__&xa=__ADPLATFORM__&tr=__REQUESTID__&mo=__OS__&m0=__OPENUDID__&m0a=__DUID__&m1=__ANDROIDID1__&m1a=__ANDROIDID__&m2=__IMEI__&m4=__AAID__&m5=__IDFA__&m6=__MAC1__&m6a=__MAC__&o="/> </div><script src='//log.51cto.com/imp_count.php?adder=weiruixin&date=2017-11-08&adpos=network article pip-1'></script><img src="" border="0" width="0" height="0" /><div><img height="0" width="0" src="http://m2593.oadz.com/mp;C;2593;386;EHNy6dXawuZGdWnWinajqqEG/tQ=?m1612.oadz.com/mp;C;1612;768;rT+i4UkMR9SFl+ZV3FqHZk2IlIw=?*"/> </div><script src='//log.51cto.com/imp_count.php?adder=BaoDe&date=2017-11-08&adpos=Server banner-2'></script><img src="" border="0" width="0" height="0" /><div><img height="0" width="0" src="http://m2593.oadz.com/mp;C;2593;781;39KNBy6Uh4En7PCk4MmXMbKbv/o=?m1570.oadz.com/mp;C;1570;1400;ww9DSX5uNKl1V2pj9M2B/BBiWEg=?*"/> </div><script src='//log.51cto.com/imp_count.php?adder=shuguang&date=2017-11-08&adpos=stor banner-1'></script><img src="" border="0" width="0" height="0" /><div><img height="0" width="0" src="http://m2593.oadz.com/mp;C;2593;781;39KNBy6Uh4En7PCk4MmXMbKbv/o=?m1570.oadz.com/mp;C;1570;1400;ww9DSX5uNKl1V2pj9M2B/BBiWEg=?*"/> </div><script src='//log.51cto.com/imp_count.php?adder=shuguang&date=2017-11-08&adpos=stor banner-2'></script><script>this.status='Íê³É'</script><script>
+function refreshall(){
+	for(var i=0;i < 0;i++){
+		document.getElementById("ifr_"+i).src='http://www.51cto.com/blank.html';
+	}
+}
+</script>
+<script>
+window.onload = function(){
+setTimeout("refreshall()",5000);
+}
+</script>
